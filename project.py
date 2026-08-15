1bec241e07079c94a41c71c22f557fd56c61bd82
veronikayushchak-coder <veronikayushchak1@gmail.com>
2026-08-15 12:40:16 -0400

    Update project.py

--- project.py
+++ project.py
@@ -2,8 +2,45 @@ import streamlit as st
 import pandas as pd
 
 
+# Page configuration
 st.set_page_config(
     page_title="LifeLens",
     page_icon="🌎",
     layout="wide"
+)
+
+
+# Life expectancy dataset
+LIFE_URL = (
+    "https://raw.githubusercontent.com/"
+    "veronikayushchak-coder/dsc205-streamlit-lifelens/"
+    "main/life-expectancy.csv"
+)
+
+
+# Load the data
+@st.cache_data
+def load_data():
+    life = pd.read_csv(LIFE_URL)
+    return life
+
+
+df = load_data()
+
+
+# App title
+st.title("🌎 LifeLens")
+
+st.subheader(
+    "Understanding how the world lives."
+)
+
+st.write(
+    "Explore life expectancy around the world."
+)
+
+
+# Preview the data
+st.dataframe(
+    df.head(10)
 )
